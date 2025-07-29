343764e5e90ce5ccf6b1e08d3cf9d0f22a615883
faqihfnf <faqih.fnf@gmail.com>
2025-07-30 00:07:49 +0700

    add global error handling

--- src/components/provider.tsx
+++ src/components/provider.tsx
@@ -2,20 +2,25 @@
 
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 import { ReactNode } from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 type ProvidersProps = {
   children: ReactNode;
 };
 const Providers = ({ children }: ProvidersProps) => {
   return (
-    <NextThemesProvider
-      attribute="class"
-      defaultTheme="system"
-      enableSystem
-      disableTransitionOnChange
-    >
-      {children}
-    </NextThemesProvider>
+    <QueryClientProvider client={queryClient}>
+      <NextThemesProvider
+        attribute="class"
+        defaultTheme="system"
+        enableSystem
+        disableTransitionOnChange
+      >
+        {children}
+      </NextThemesProvider>
+    </QueryClientProvider>
   );
 };
 

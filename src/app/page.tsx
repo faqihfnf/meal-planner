4a642692e29185d9988ebaedfb78eedea8d32eb4
faqihfnf <faqih.fnf@gmail.com>
2025-07-29 17:34:17 +0700

    install shadcn, prettier for tailwind and create route admin

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,3 @@
-import db from "@/lib/db";
-
 export default async function Home() {
-  const user = await db.user.findFirst();
-
-  return <h1>{JSON.stringify(user)}</h1>;
+  return <h1>Home</h1>;
 }
